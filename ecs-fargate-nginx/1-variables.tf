68680d0c6fe0f57a723f8376c1ea919a7020587d
Juan Roldan <juanroldan1989@gmail.com>
2024-09-27 20:55:00 +0200

    ECS (FARGATE) Service AutoScaling - config added

--- ecs-fargate-nginx/1-variables.tf
+++ ecs-fargate-nginx/1-variables.tf
@@ -1,6 +1,7 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+variable "vpc_cidr" {
+  description = "CIDR block for main"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "env" {
@@ -8,18 +9,22 @@ variable "env" {
   default = "development"
 }
 
-variable "app_name" {
+variable "aws_region" {
   type    = string
-  default = "sample-ecs-fargate-app"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for main"
-  type        = string
-  default     = "10.0.0.0/16"
+  default = "us-east-1"
 }
 
 variable "availability_zones" {
   type    = string
   default = "us-east-1a"
+}
+
+variable "app_name" {
+  type    = string
+  default = "sample-ecs-fargate-app"
+}
+
+variable "ecs_auto_scale_role_name" {
+  description = "ECS auto scale role name"
+  default     = "myEcsAutoScaleRole"
 }

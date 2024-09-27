e86cb4132e35d9214b6fe5a00a9f4d5f4289a7b7
Juan Roldan <juanroldan1989@gmail.com>
2024-09-27 18:43:24 +0200

    ECS (FARGATE) config files added

---
+++ ecs-fargate-nginx/1-variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "env" {
+  type    = string
+  default = "development"
+}
+
+variable "app_name" {
+  type    = string
+  default = "sample-ecs-fargate-app"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for main"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  type    = string
+  default = "us-east-1a"
+}

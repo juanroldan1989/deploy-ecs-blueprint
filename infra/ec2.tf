67e6776e4d214a3b5df37cc4eb4f8c7a9866c3b1
Juan Roldan <juanroldan1989@gmail.com>
2024-09-27 15:47:39 +0200

    EC2 instances and ASG configuration added

---
+++ infra/ec2.tf
@@ -0,0 +1,39 @@
+data "aws_ami" "this" {
+  most_recent = "true"
+  owners      = ["self", "amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-2.0.????????-x86_64-ebs"]
+  }
+}
+
+resource "aws_launch_template" "ecs_lt" {
+  name_prefix   = "ecs-template"
+  image_id      = data.aws_ami.this.image_id
+  instance_type = "t3.micro"
+
+  # key_name               = "ec2ecsglog"
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+
+  iam_instance_profile {
+    name = "ecsInstanceRole"
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 30
+      volume_type = "gp2"
+    }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
+
+  user_data = filebase64("${path.module}/ecs.sh")
+}

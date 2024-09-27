adba310956e74d7d8098470eb9a76cbf01282b08
Juan Roldan <juanroldan1989@gmail.com>
2024-09-27 19:18:16 +0200

    ECS (FARGATE) - Logs and Task Template added

--- ecs-fargate-nginx/7-ecs-task.tf
+++ ecs-fargate-nginx/7-ecs-task.tf
@@ -1,3 +1,18 @@
+data "template_file" "cb_app" {
+  template = file("./templates/app.json.tpl")
+
+  vars = {
+    app_name       = "nginx"
+    app_image      = "public.ecr.aws/nginx/nginx:1.27.1-alpine3.20-perl"
+    fargate_cpu    = 256
+    fargate_memory = 256
+    essential      = true
+    app_port       = 80
+    aws_log_group  = "/ecs/log-group/${var.app_name}/${var.env}"
+    aws_region     = var.aws_region
+  }
+}
+
 resource "aws_ecs_task_definition" "ecs_task_definition" {
   family                   = "my-ecs-fargate-task"
   network_mode             = "awsvpc"
@@ -5,26 +20,10 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   execution_role_arn       = "arn:aws:iam::542979624611:role/ecsTaskExecutionRole"
   cpu                      = 256
   memory                   = 512
+  container_definitions    = data.template_file.cb_app.rendered
 
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture        = "X86_64"
   }
-
-  container_definitions = jsonencode([
-    {
-      name      = "nginx"
-      image     = "public.ecr.aws/nginx/nginx:1.27.1-alpine3.20-perl"
-      cpu       = 256
-      memory    = 256
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-          protocol      = "tcp"
-        }
-      ]
-    }
-  ])
 }

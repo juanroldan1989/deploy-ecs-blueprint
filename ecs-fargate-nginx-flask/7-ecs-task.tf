aa1518b41fc684308f700c4cf18d2b31b91d6c9f
Juan Roldan <juanroldan1989@gmail.com>
2024-09-28 18:03:24 +0200

    ECS (FARGATE) NGINX with Flask App - ECS Task error fixed
    - `gunicorn` server launched within Docker image instead of through ECS Task command

--- ecs-fargate-nginx-flask/7-ecs-task.tf
+++ ecs-fargate-nginx-flask/7-ecs-task.tf
@@ -19,6 +19,9 @@ resource "aws_ecs_task_definition" "custom_nginx_flask_task" {
       environment = [
         { "name" : "FLASK_SERVER_ADDR", "value" : "flask-app:8000" }
       ]
+      dependsOn = [
+        { "containerName" : "flask-app", "condition" : "HEALTHY" }
+      ]
       logConfiguration = {
         logDriver = "awslogs"
         options = {
@@ -41,7 +44,20 @@ resource "aws_ecs_task_definition" "custom_nginx_flask_task" {
       memory      = 256
       essential   = true
       networkMode = "awsvpc"
-      command     = ["gunicorn -w 3 -t 60 -b 0.0.0.0:8000 app:app"]
+
+      # To solve issue:
+      # runc create failed: unable to start container process: exec: "gunicorn -w 3 -t 60 -b 0.0.0.0:8000 app:app": executable file not found in $PATH
+      # command     = ["gunicorn -w 3 -t 60 -b 0.0.0.0:8000 app:app"]
+
+      # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/example_task_definitions.html#example_task_definition-containerdependency
+      # healtCheck needs to be defined here since another container depends on this container being healthy
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl --silent --fail http://localhost:8000/flask-health-check || exit 1"]
+        interval    = 10
+        retries     = 3
+        timeout     = 10
+        startPeriod = 30
+      }
       logConfiguration = {
         logDriver = "awslogs"
         options = {

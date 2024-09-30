0f45853fed86f4d86d5c1de963cc8a00f5b071aa
Juan Roldan <juanroldan1989@gmail.com>
2024-09-30 18:26:23 +0200

    App 4 - DB Connection python code sorted out properly

--- 4.ecs-fargate-nginx-flask-db/flask/app.py
+++ 4.ecs-fargate-nginx-flask-db/flask/app.py
@@ -1,34 +1,35 @@
+import os
 from flask import Flask
 import mysql.connector as mariadb
 
 class DBManager:
-  def __init__(self, database="example", host="db", user="root", password_file=None):
+  def __init__(self, host="db", database="example", user="root", password_file=None):
     pf = open(password_file, 'r')
     self.connection = mariadb.connect(
-      user=user,
+      host=os.getenv('DB_HOST', host),  # database service/container
+      database=os.getenv('DB_NAME', database),
+      user=os.getenv('DB_USER', user),
       password=pf.read(),
-      host=host, # name of the mysql service as set in the docker compose file
-      database=database,
       auth_plugin='mysql_native_password'
     )
     pf.close()
     self.cursor = self.connection.cursor()
 
   def populate_db(self):
-    self.cursor.execute('DROP TABLE IF EXISTS blog')
-    self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
-    self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
+    self.cursor.execute('DROP TABLE IF EXISTS articles')
+    self.cursor.execute('CREATE TABLE articles (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
+    self.cursor.executemany('INSERT INTO articles (id, title) VALUES (%s, %s);', [(i, 'Article post #%d'% i) for i in range (1,5)])
     self.connection.commit()
 
-  def query_titles(self):
-    self.cursor.execute('SELECT title FROM blog')
+  def get_articles(self):
+    self.cursor.execute('SELECT title FROM articles')
     titles = []
     for c in self.cursor:
       titles.append(c[0])
     return titles
 
-  def get_title(self, id):
-    self.cursor.execute(f"SELECT title FROM blog WHERE id = {id}")
+  def get_article(self, id):
+    self.cursor.execute(f"SELECT title FROM articles WHERE id = {id}")
     titles = []
     for c in self.cursor:
       titles.append(c[0])
@@ -42,21 +43,20 @@ def get_db():
   if not conn:
     conn = DBManager(password_file='/run/secrets/db-password')
     conn.populate_db()
-
   return conn
 
 @app.route('/', methods=['GET'])
-def list_titles():
+def home():
   conn = get_db()
-  titles = conn.query_titles()
+  titles = conn.get_articles()
   response = ''
   for title in titles:
     response = response  + '<h2>   Article  ' + title + '</h2>'
   return response
 
 @app.route('/<int:title_id>', methods=['GET'])
-def get_title(title_id):
+def get_article(title_id):
   conn = get_db()
-  title = conn.get_title(id=title_id)
+  title = conn.get_article(id=title_id)
   response = f"<h1> Article - {title} </h1>"
   return response
